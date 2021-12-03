83a3c38afd8112791e1074bb3af2a1dd9c57f974
Temirlan Rsanov <86775981+temirlanr@users.noreply.github.com>
2021-12-04 00:11:46 +0600

    fix

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ def start(update, context):
 
 def wishlist(update: Update, context: CallbackContext) -> int:
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="WHat do you want from santa my dear?")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="What do you want from santa my dear?")
 
     return WISHLIST 
 
@@ -42,7 +42,7 @@ def wishlist_handler(update: Update, context: CallbackContext) -> int:
 
     user = update.message.from_user
     logger.info("User %s wants: %s", user.username, update.message.text)
-    cur.execute(f"INSERT INTO main_info (user_id, username, wishlist) VALUES ({user.id}, {str(user.username)}, {str(update.message.text)});")
+    cur.execute(f"INSERT INTO main_info VALUES ({user.id}, {str(user.username)}, {'NULL'}, {str(update.message.text)});")
     update.message.reply_text('Now tell me how to call you!')
 
     return NAME
@@ -52,7 +52,7 @@ def define_name(update: Update, context: CallbackContext) -> int:
 
     user = update.message.from_user
     logger.info("User %s is called %s", user.username, update.message.text)
-    cur.execute(f"INSERT INTO main_info (name) VALUES ({str(update.message.text)});")
+    cur.execute(f"UPDATE main_info SET name = {str(update.message.text)} WHERE user_id = {user.id};")
     update.message.reply_text('You for sure will get an amazing present from santa!')
 
     return ConversationHandler.END
