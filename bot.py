384a917b4f1de99a9cd3c8b785d4693b62405f45
Temirlan Rsanov <86775981+temirlanr@users.noreply.github.com>
2021-12-04 00:24:19 +0600

    fix

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def wishlist_handler(update: Update, context: CallbackContext) -> int:
 
     user = update.message.from_user
     logger.info("User %s wants: %s", user.username, update.message.text)
-    cur.execute(f"INSERT INTO main_info VALUES ({user.id}, {user.username}, {'NULL'}, {update.message.text});")
+    cur.execute(f"INSERT INTO main_info VALUES ({user.id}, {str(user.username)}, {'NULL'}, {update.message.text});")
     update.message.reply_text('Now tell me how to call you!')
 
     return NAME
